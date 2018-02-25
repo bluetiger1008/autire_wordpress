1f58f586c0ca310e9535b1ede0692b2f9fa64e17
iam2611008 <dragonwarrior1008@yandex.com>
2018-02-25 10:29:18 -0800

    contact us page

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,9 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  	<a class="logo" href="<?= esc_url(home_url('/')); ?>"><img src="<?= get_template_directory_uri(); ?>/dist/images/pricekubecka_logo.png"></a>
+  	<div class="footer-menu">
+	    <?php dynamic_sidebar('sidebar-footer'); ?>
+	  </div>
+	  <p class="copyright">© 2018 PriceKubecka</p>
   </div>
 </footer>
